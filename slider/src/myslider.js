724e72df709dee917042ca13313df8af7f0980ac
SethDonohue <donohue.seth@gmail.com>
2018-08-07 11:48:22 -0700

    removed ghost code

--- slider/src/myslider.js
+++ slider/src/myslider.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 class MySlider extends Component {
   handleChange = (event) => {
-    // event.preventDefault;
     const selectionValue = event.target.value;
     this.props.selectionUpdate(selectionValue);
   }
